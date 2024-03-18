902c0c030be77b1a81d4203d934b25b912b9d268
ACORRE2 <audren.corre@ynov.com>
2024-03-18 09:43:43 +0100

    update jsontosql

--- json_tosql.py
+++ json_tosql.py
@@ -1,14 +1,44 @@
 import pandas as pd
 import sqlite3
-df = pd.read_json('data.json', )
-df.columns = df.iloc[0]
-df = df[1:]
-df.drop(df[df['t'] == 'mq'].index, inplace = True)
-df['t'] = df['t'].astype(float) - 273.15
-data = df[['numer_sta','date','ff','t','u']]
-data.loc[:,'date'] = pd.to_datetime(data['date'])
-data.columns = ['NUM_STATION', 'DATE', 'VITESSE_VENT', 'TEMPERATURE', 'HUMIDITE']
 
-data.to_sql('meteo', sqlite3.connect('data.db'), if_exists='replace', index=False)
+def formatting():
+    """
+    Format the data from the json file and save it in a pandas DataFrame
+    Parameters
+    ---
+    None
+    
+    Returns
+    ---
+    pd.DataFrame
+        The formatted data
+    """
+    df = pd.read_json('data.json', )
+    df.columns = df.iloc[0]
+    df = df[1:]
+    df.drop(df[df['t'] == 'mq'].index, inplace = True)
+    df['t'] = df['t'].astype(float) - 273.15
+    data = df[['numer_sta','date','ff','t','u']]
+    data.loc[:,'date'] = pd.to_datetime(data['date'])
+    data.columns = ['NUM_STATION', 'DATE', 'VITESSE_VENT', 'TEMPERATURE', 'HUMIDITE']
+    return data
+
+def to_sql(data):
+    """
+    Save the data in a sqlite database
+
+    Parameters
+    ---
+    data : pd.DataFrame
+        The data to save
+
+    Returns
+    ---
+    None
+    """
+
+    conn = sqlite3.connect('data.db')
+    data.to_sql('meteo', conn, if_exists='replace', index = False)
+
 
 print(pd.read_sql('SELECT * FROM meteo', sqlite3.connect('data.db')))
